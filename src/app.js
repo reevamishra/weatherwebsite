c9345cc04021d73b6bac54792c4b9ea58c11d148
reeva9 <36332230+reeva9@users.noreply.github.com>
2021-01-26 20:43:54 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ console.log(path.join(__dirname,'../public'))
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname, "../templates/views")
@@ -101,6 +103,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("The server started correctly");
 });
